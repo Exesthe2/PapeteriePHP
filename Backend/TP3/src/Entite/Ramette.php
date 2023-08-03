1214af13f891cc056aa00fb00c01426c10b45698
Exesthe2 <esteban.fournier2022@campus-eni.fr>
2023-08-03 16:46:00 +0200

    add tp3

---
+++ Backend/TP3/src/Entite/Ramette.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entite;
+
+class Ramette extends Article
+{
+    public function __construct(
+        string $marque,
+        string $reference,
+        string $designation,
+        float $prixUnitaire,
+        int $qteStock,
+        private int $grammage,
+        ?int $idArticle = null,
+    ) {
+        parent::__construct($marque, $reference, $designation, $prixUnitaire, $qteStock, $idArticle);
+    }
+
+    public function __toString()
+    {
+        return sprintf(
+            "%s Ramette [grammage=%s]\n",
+            parent::__toString(),
+            $this->grammage
+        );
+    }
+    public function getGrammage(): int
+    {
+        return $this->grammage;
+    }
+
+    public function setGrammage(int $grammage): self
+    {
+        $this->grammage = $grammage;
+
+        return $this;
+    }
+
+    public static function fromState(array $state): Ramette
+    {
+        return new self(
+            $state['marque'],
+            $state['reference'],
+            $state['designation'],
+            $state['prix_unitaire'],
+            $state['qte_stock'],
+            $state['grammage'],
+            $state['id_article'],
+        );
+    }
+}
